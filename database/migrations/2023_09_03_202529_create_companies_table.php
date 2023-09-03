9f4563ee6a6ff4db6c604105f40abcff9fee41b8
Gabriel Vitor <gabrielvitordasilva@gmail.com>
2023-09-03 17:32:25 -0300

    migrations

--- database/migrations/2023_09_03_202529_create_companies_table.php
+++ database/migrations/2023_09_03_202529_create_companies_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->string('name');
+            $table->string('cpf_cnpj', 14);
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('companies');
     }
 };
