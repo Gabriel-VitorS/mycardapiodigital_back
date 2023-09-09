d7ca24b8b65360f5b4cacc48251523be19f6ed02
Gabriel Vitor <gabrielvitordasilva@gmail.com>
2023-09-09 16:42:31 -0300

    Métodos para gerar link das imagens

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,12 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    
+    public function getUrlLogoImage( string | null $image){
+        return $image != null ? url('/') . '/storage/logo_image/' . $image : url('/') . '/storage/logo_image/padrao.png';
+    }
+
+    public function getUrlBannerImage( string | null $image){
+        return $image != null ? url('/') . '/storage/banner_image/' . $image : url('/') . '/storage/banner_image/padrao.png';
+    }
 }
